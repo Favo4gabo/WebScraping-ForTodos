6a3b3c327fe7d13e67a21d0483dc9328dd9a65aa
Gabriel Franco <gabrielnicolasfranco4@gmail.com>
2024-07-03 15:09:44 -0400

    pototipo

--- app.py
+++ app.py
@@ -4,109 +4,6 @@ import pandas
 import requests
 from bs4 import BeautifulSoup
 
-# Crear una instancia de la aplicación Flask
-app = Flask(__name__)
-
-# Ruta para la página principal que muestra el formulario
-@app.route('/')
-def index():
-    # Renderiza el formulario HTML
-    return render_template_string(open('index.html').read())
-
-
-# Ruta para procesar los datos enviados desde el formulario (automatico)
-@app.route('/automatico', methods=['POST'])
-def automatico():
-    # Obtener las URLs ingresadas en el formulario y dividirlas en una lista
-    file = request.files['file']
-    
-    # Guardar un archivo
-    # file.save(file.filename)
-    
-    workbook = openpyxl.load_workbook(file)
-    sheet = workbook['Links']  # Cambia el nombre de la hoja según sea necesario
-
-    data = []
-
-    # Recorre las filas y extrae los hipervínculos
-    for row in sheet.iter_rows():
-        cell = row[2]  # Cambia el índice de columna según sea necesario
-        if cell.hyperlink:            
-            urls = cell.hyperlink.target
-            # nombre = cell.value
-                                        
-            # # Agregar las urls extraídas a la lista            
-            data.append(urls)
-
-    print(data)
-    
-    for url in data:
-        response = requests.get(url.strip())  # Realizar la solicitud HTTP a la URL
-        soup = BeautifulSoup(response.content, 'html.parser')  # Parsear el contenido HTML
-        
-        # Extraer el nombre y el precio del producto usando selectores CSS
-        # Ajusta los selectores según la estructura de la página de Mercado Libre
-        nombre_tag = soup.select_one('h1')
-        if nombre_tag is not None:
-            nombre = nombre_tag.text.strip()
-        precio_tag = soup.select_one('span.andes-money-amount__fraction')
-        if precio_tag is not None:
-            precio = precio_tag.text.strip()
-        condicion_tag = soup.select_one('span.ui-pdp-subtitle')
-        if condicion_tag is not None:
-            condicion = condicion_tag.text.strip()
-        
-        # Agregar los datos extraídos a la lista
-        data.append({'Nombre del producto': nombre, 'Precio': precio, 'Condicion':condicion})
-        print(data)
-
-    # Crear un DataFrame de pandas con los datos extraídos
-    df = pandas.DataFrame(data)
-    # Guardar el DataFrame en un archivo Excel
-    df.to_excel('productos_2°Intento.xlsx', index=False)
-
-    # Devolver un mensaje de éxito
-    return 'Archivo Excel generado con éxito (Automatico).'
-
-
-# Ruta para procesar los datos enviados desde el formulario
-@app.route('/manual', methods=['POST'])
-def manual():
-    # Obtener las URLs ingresadas en el formulario y dividirlas en una lista
-    urls = request.form['urls'].split('\n')
-    data = []
-
-    # Iterar sobre cada URL para extraer la información del producto
-    for url in urls:
-        response = requests.get(url.strip())  # Realizar la solicitud HTTP a la URL
-        soup = BeautifulSoup(response.content, 'html.parser')  # Parsear el contenido HTML
-        
-        # Extraer el nombre y el precio del producto usando selectores CSS
-        # Ajusta los selectores según la estructura de la página de Mercado Libre
-        nombre = soup.select_one('h1').text if soup.select_one('h1') else 'N/A'
-        precio = soup.select_one('span.andes-money-amount__fraction').text if soup.select_one('span.andes-money-amount__fraction') else 'N/A'
-        condicion = soup.select_one("span.ui-pdp-subtitle").text if soup.select_one("span.ui-pdp-subtitle") else 'N/A'
-        
-        # Agregar los datos extraídos a la lista
-        data.append({'Nombre del producto': nombre, 'Precio': precio, "Condicion": condicion})
-
-    # Crear un DataFrame de pandas con los datos extraídos
-    df = pandas.DataFrame(data)
-    # Guardar el DataFrame en un archivo Excel
-    df.to_excel('productos.xlsx', index=False)
-
-    # Devolver un mensaje de éxito
-    return 'Archivo Excel generado con éxito (Manual).'
-    
-if __name__ == '__main__':
-    app.run(debug=True)
-    
-
-
-# # ------------------------------------------------------------------------------
-# # CODIGO POR PARTES
-# # ------------------------------------------------------------------------------
-    
 # # Crear una instancia de la aplicación Flask
 # app = Flask(__name__)
 
@@ -117,9 +14,65 @@ if __name__ == '__main__':
 #     return render_template_string(open('index.html').read())
 
 
+# # Ruta para procesar los datos enviados desde el formulario (automatico)
+# @app.route('/automatico', methods=['POST'])
+# def automatico():
+#     # Obtener las URLs ingresadas en el formulario y dividirlas en una lista
+#     file = request.files['file']
+#     # urls = request.form['urls'].split('\n')
+    
+#     # Guardar un archivo
+#     # file.save(file.filename)
+    
+#     workbook = openpyxl.load_workbook(file)
+#     sheet = workbook['Links']  # Cambia el nombre de la hoja según sea necesario
+
+#     data = []
+
+#     # Recorre las filas y extrae los hipervínculos
+#     for row in sheet.iter_rows():
+#         cell = row[2]  # Cambia el índice de columna según sea necesario
+#         if cell.hyperlink:            
+#             urls = cell.hyperlink.target
+#             # nombre = cell.value
+                                        
+#             # # Agregar las urls extraídas a la lista            
+#             data.append(urls)
+
+#     print(data)
+    
+#     for url in data:
+#         response = requests.get(url)  # Realizar la solicitud HTTP a la URL
+#         soup = BeautifulSoup(response.content, 'html.parser')  # Parsear el contenido HTML
+        
+#         # Extraer el nombre y el precio del producto usando selectores CSS
+#         # Ajusta los selectores según la estructura de la página de Mercado Libre
+#         nombre_tag = soup.select_one('h1')
+#         if nombre_tag is not None:
+#             nombre = nombre_tag.text.strip()
+#         precio_tag = soup.select_one('span.andes-money-amount__fraction')
+#         if precio_tag is not None:
+#             precio = precio_tag.text.strip()
+#         condicion_tag = soup.select_one('span.ui-pdp-subtitle')
+#         if condicion_tag is not None:
+#             condicion = condicion_tag.text.strip()
+        
+#         # Agregar los datos extraídos a la lista
+#         data.append({'Nombre del producto': nombre, 'Precio': precio, 'Condicion':condicion})
+#         print(data)
+
+#     # Crear un DataFrame de pandas con los datos extraídos
+#     df = pandas.DataFrame(data)
+#     # Guardar el DataFrame en un archivo Excel
+#     df.to_excel('productos_2°Intento.xlsx', index=False)
+
+#     # Devolver un mensaje de éxito
+#     return 'Archivo Excel generado con éxito (Automatico).'
+
+
 # # Ruta para procesar los datos enviados desde el formulario
-# @app.route('/procesar', methods=['POST'])
-# def procesar():
+# @app.route('/manual', methods=['POST'])
+# def manual():
 #     # Obtener las URLs ingresadas en el formulario y dividirlas en una lista
 #     urls = request.form['urls'].split('\n')
 #     data = []
@@ -144,38 +97,92 @@ if __name__ == '__main__':
 #     df.to_excel('productos.xlsx', index=False)
 
 #     # Devolver un mensaje de éxito
-#     return 'Archivo Excel generado con éxito.'
-
-
-# # Ruta para extraer hipervínculos de un archivo Excel y guardarlos en otro archivo Excel
-# @app.route('/extract_links')
-# def extract_links():
-#     # Abre el libro de Excel
-#     workbook = openpyxl.load_workbook('data.xlsx')
-#     sheet = workbook['Links']  # Cambia el nombre de la hoja según sea necesario
-
-#     data = []
-
-#     # Recorre las filas y extrae los hipervínculos
-#     for row in sheet.iter_rows():
-#         cell = row[2]  # Cambia el índice de columna según sea necesario
-#         if cell.hyperlink:
-#             nombre = cell.value
-#             urls = cell.hyperlink.target
-
-#             # Agregar los datos extraídos a la lista
-#             data.append({'Nombre del producto': nombre, 'URL': urls})
-
-#     # Crear un DataFrame de pandas con los datos extraídos
-#     df = pandas.DataFrame(data)
-#     # Guardar el DataFrame en un archivo Excel
-#     df.to_excel('productos.xlsx', index=False)
-
-#     return "Precios extraídos y guardados en 'productos.xlsx'"
-
-# # Ejecutar la aplicación Flask
+#     return 'Archivo Excel generado con éxito (Manual).'
+    
 # if __name__ == '__main__':
 #     app.run(debug=True)
+    
+
+
+# # ------------------------------------------------------------------------------
+# # CODIGO POR PARTES
+# # ------------------------------------------------------------------------------
+    
+# Crear una instancia de la aplicación Flask
+app = Flask(__name__)
+
+# Ruta para la página principal que muestra el formulario
+@app.route('/')
+def index():
+    # Renderiza el formulario HTML
+    return render_template_string(open('index.html').read())
+
+
+# Ruta para procesar los datos enviados desde el formulario
+@app.route('/procesar', methods=['POST'])
+def procesar():
+    # Obtener las URLs ingresadas en el formulario y dividirlas en una lista
+    urls = request.form['urls'].split('\n')
+    data = []
+
+    # Iterar sobre cada URL para extraer la información del producto
+    for url in urls:
+        response = requests.get(url.strip())  # Realizar la solicitud HTTP a la URL
+        soup = BeautifulSoup(response.content, 'html.parser')  # Parsear el contenido HTML
+        
+        # Extraer el nombre y el precio del producto usando selectores CSS
+        # Ajusta los selectores según la estructura de la página de Mercado Libre
+        nombre = soup.select_one('h1').text if soup.select_one('h1') else 'N/A'
+        precio = soup.select_one('span.andes-money-amount__fraction').text if soup.select_one('span.andes-money-amount__fraction') else 'N/A'
+        condicion = soup.select_one("span.ui-pdp-subtitle").text if soup.select_one("span.ui-pdp-subtitle") else 'N/A'
+        categoria = soup.select_one('a.andes-breadcrumb__item').text if soup.select_one('a.andes-breadcrumb__item') else 'N/A'
+        descripcion = soup.select_one('p.ui-pdp-description__content').text if soup.select_one('p.ui-pdp-description__content') else 'N/A'
+        
+        # Agregar los datos extraídos a la lista
+        data.append({'Nombre del producto': nombre, 'Precio': precio, "Condicion": condicion, 'Categoria': categoria, 'Descripcion':descripcion})
+
+    # Crear un DataFrame de pandas con los datos extraídos
+    df = pandas.DataFrame(data)
+    # Guardar el DataFrame en un archivo Excel
+    df.to_excel('productos.xlsx', index=False)
+    
+    print(data)
+
+    # Devolver un mensaje de éxito
+    return 'Archivo Excel generado con éxito.'
+
+
+# Ruta para extraer hipervínculos de un archivo Excel y guardarlos en otro archivo Excel
+@app.route('/extract_links', methods=['POST'])
+def extract_links():
+    # Abre el libro de Excel
+    workbook = openpyxl.load_workbook('data - copia.xlsx')
+    sheet = workbook['Links']  # Cambia el nombre de la hoja según sea necesario
+
+    data = []
+
+    # Recorre las filas y extrae los hipervínculos
+    for row in sheet.iter_rows():
+        cell = row[2]  # Cambia el índice de columna según sea necesario
+        if cell.hyperlink:
+            nombre = cell.value
+            urls = cell.hyperlink.target
+
+            # Agregar los datos extraídos a la lista
+            data.append({'Nombre del producto': nombre, 'URL': urls})
+
+    # Crear un DataFrame de pandas con los datos extraídos
+    df = pandas.DataFrame(data)
+    # Guardar el DataFrame en un archivo Excel
+    df.to_excel('productos.xlsx', index=False)
+    
+    print(data)
+
+    return "Precios extraídos y guardados en 'productos.xlsx'"
+
+# Ejecutar la aplicación Flask
+if __name__ == '__main__':
+    app.run(debug=True)
 
     
 
